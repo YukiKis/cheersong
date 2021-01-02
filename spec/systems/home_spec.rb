8b6da8385ba0865cbfdc1235cef90f6140ceefbf
EC2 Default User <ec2-user@ip-172-31-34-252.ec2.internal>
2021-01-02 14:29:28 +0000

    write rspecs

--- spec/systems/home_spec.rb
+++ spec/systems/home_spec.rb
@@ -1,17 +1,17 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Home page", type: :system do
-  context "on home page" do
+RSpec.describe 'Home page', type: :system do
+  context 'on home page' do
     before do
       visit root_path
     end
-#    it "has some tags" do
+#    it 'has some tags' do
 #    end
-#    it "has search form for songs" do
+#    it 'has search form for songs' do
 #    end
-#    it "has some songs registered" do
+#    it 'has some songs registered' do
 #    end
   end
-  context "on about page" do
+  context 'on about page' do
   end
 end
--- spec/systems/sidebar_spec.rb
+++ spec/systems/sidebar_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
-RSpec.desceibe "Sidebar", type: :system do
-  context "when not logged in" do
+RSpec.desceibe 'Sidebar', type: :system do
+  context 'when not logged in' do
   end
-  context "when logged in as user" do
+  context 'when logged in as user' do
   end
 end
