fb4dc9c23cd5bfe063d70f003ede26e00ee016a9
EC2 Default User <ec2-user@ip-172-31-34-252.ec2.internal>
2021-01-02 15:00:11 +0000

    write rspec for user-registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  scope module: :public do
+
+  end
+  devise_for :users, controllers: {
+      sessions: "public/users/sessions",
+      registrations: "users/registrations"
+    }
   root "home#home"
   get '/about', to: "home#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
---
+++ spec/systems/users_devise_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper" 
+
+RSpec.describe "User-devise page", type: :system do
+  context "on register-new" do
+    before do
+      visit new_user_registration_path
+    end
+    it "has form for name" do
+      expect(page).to have_field "user[name]"
+    end
+    it "has form for email" do
+      expect(page).to have_field "user[email]"
+    end
+    it "has form for password" do
+      expect(page).to have_field "user[password]"
+    end
+    it "has form for password_confirmation" do
+      expect(page).to have_field "user[password_confirmation"
+    end
+    it "has button to submit" do
+      expect(page).to have_button "登録"
+    end
+    it "succeeds to register" do
+      fill_in "user[name]", with: "yuki"
+      fill_in "user[email]", with: "yuki@com"
+      fill_in "user[password]", with: "testtest"
+      fill_in "user[password_confirmation]", with: "testtest"
+      expect{ click_button "登録" }.to change{ User.all.count }.by(1)
+      expect(page).to have_content "successfully"
+    end
+    it "fails to register" do
+      click_button "登録"
+      expect(page).to have_content "エラー"
+    end
+  end
+end
