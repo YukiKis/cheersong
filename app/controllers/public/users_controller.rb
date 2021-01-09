e899383c4df290160ac8d8339a3d07caadbbc62b
EC2 Default User <ec2-user@ip-172-31-34-252.ec2.internal>
2021-01-09 12:50:53 +0000

    making song-favorite on user-index nd show

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -6,6 +6,7 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @songs = @user.songs
     if @user != current_user
       @room = current_user.has_room?(@user)
     end
